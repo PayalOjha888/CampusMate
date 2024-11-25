c919d19fd46d394ee70c735653cfb9dd538ef965
Muskan Dadhich <137093533+muskandz@users.noreply.github.com>
2024-11-25 21:32:46 +0530

    Delete scripts directory

--- scripts/pdfToBinary.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-function readPdfFile(filePath) {
-    return fs.readFileSync(filePath);
-}
-
-//example usage:
-const pdfPath = 'path/to/your/notes.pdf';
-const binaryData = readPdfFile(pdfPath);
